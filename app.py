7a7c90f09fe7404a4886c5bf06321145613fba7b
vagrant <vagrant@vagrant.vm>
2018-12-07 06:43:56 +0000

    Added functional input field to create new hallmark

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db_setup import Base, AgingHallmark
@@ -22,9 +22,15 @@ def agingHallmarks():
     return render_template('agingHallmarks.html', 
                                 aging_hallmarks = aging_hallmarks)
 
-@app.route('/hallmark/new')
+@app.route('/hallmark/new', methods = ['GET', 'POST'])
 def newHallmark():
-    return render_template('newHallmark.html')
+    if request.method == 'POST':
+        newHallmark = AgingHallmark(name=request.form['name'])
+        session.add(newHallmark)
+        session.commit()
+        return redirect(url_for('agingHallmarks'))
+    else:
+        return render_template('newHallmark.html')
 
 @app.route('/hallmark/<int:hallmark_id>/edit')
 def editHallmark(hallmark_id):
